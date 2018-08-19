7b48fbdf305427559cd6aa7b6c4a89f5bd792f27
Pierce Morrill <piercemorrill91@gmail.com>
2018-08-18 23:58:00 -0500

    finishing input fields

--- src/components/input.js
+++ src/components/input.js
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const styles = {
     container: {
         display: 'flex',
+        margin: 20,
         flexWrap: 'wrap',
     },
     input: {
-        margin: '9%',
+        padding: 20,
     }, 
 };
 
@@ -17,20 +20,50 @@ function Inputs(props) {
     const { classes } = props;
     return (
         <div className={classes.container}>
+
+            <h1>
+                Create your Own Profile Here
+            </h1>
+
             <Input 
-                placeHolder='Full Name'
+                style={{ width: '25%' }}
+                placeholder='Full Name'
                 className={classes.input}
                 inputProps={{
                     'aria-label' : 'Description',
                 }}
             />
+
+            <Button 
+                accept="image/*"
+                multiple
+                type="file"
+                variant="contained" 
+                color="default" 
+                className={classes.button} 
+                style={{  margin: 20, }}
+            >
+                <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                />
+                <CloudUploadIcon className={classes.rightIcon} />
+            </Button>
+
+
+
             <Input 
-                placeHolder='Describe Yourself'
+                style={{ width: '100%', }}
+                placeholder='Describe Yourself'
                 className={classes.input}
                 inputProps={{
                     'aria-label' : 'Description',
                 }}
             />
+            
         </div>
     ) //return
 }//Inputs
